885d65993c853588eeca72b6c925ef3c02a2e425
johndward01 <johndward01@gmail.com>
2022-08-07 00:51:02 -0500

    added grpc client and server projects

---
+++ GrpcClient_Demo_v1/GrpcClient_Demo_v1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcClient_Demo_v1;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7058");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Greeting: " + reply.Message);
